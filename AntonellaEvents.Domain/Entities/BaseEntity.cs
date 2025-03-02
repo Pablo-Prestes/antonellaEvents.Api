2acb6ac3f040e9bbcaa83aa00e37645ccff459de
Pablo Prestes <pablo.henrique.prestes@gmail.com>
2025-03-01 21:54:41 -0300

    Feat(Entities) - Adicionando entidades base do projeto BaseEntity, Adress, Events

---
+++ AntonellaEvents.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AntonellaEvents.Domain.Entities
+{
+	public abstract class BaseEntity
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public Guid Uuid { get; protected set; } 
+
+		[Required]
+		public DateTime CreatedAt { get; protected set; }
+
+		[MaxLength(40)]
+		public string? UserRegistered { get; protected set; }
+
+		[MaxLength(40)]
+		public string? UserUpdated { get; protected set; }
+
+		[MaxLength(40)]
+		public string? UsuarioDeletou { get; protected set; }
+		public bool Active { get; protected set; }
+		public DateTime? Updated { get; protected set; }
+		public DateTime? Deleted { get; protected set; }
+		public void Post(string usuarioCadastrouId)
+		{
+			Uuid = Guid.NewGuid();
+			UserRegistered = usuarioCadastrouId;
+			CreatedAt = DateTime.Now;
+			Active = true;
+		}
+
+		public void Update(string usuarioAtualizouId)
+		{
+			UserUpdated = usuarioAtualizouId;
+			Updated = DateTime.Now;
+		}
+
+		public void Delete(string usuarioDeletouId)
+		{
+			UsuarioDeletou = usuarioDeletouId;
+			Deleted = DateTime.Now;
+			Active = false;
+		}
+	}
+}
---
+++ AntonellaEvents.Domain/Entities/Events.cs
@@ -0,0 +1,22 @@
+﻿namespace AntonellaEvents.Domain.Entities
+{
+   public  class Events : BaseEntity
+    {
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public DateTime StartDate { get; set; }
+
+		public DateTime EndDate { get; set; }
+
+		public string Location { get; set; }
+
+		public bool IsPublic { get; set; }
+
+		public Address? Address { get; set; }
+
+		public int AddresId { get; set; }
+	}
+}
