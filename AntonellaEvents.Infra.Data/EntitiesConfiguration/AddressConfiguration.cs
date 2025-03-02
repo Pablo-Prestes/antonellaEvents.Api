63041ecd9247ca0e6fd961e108eaa50fc88f1bd2
Pablo Prestes <pablo.henrique.prestes@gmail.com>
2025-03-01 21:56:36 -0300

    Feat (Data) - Adicionando Adress e Events configuration

---
+++ AntonellaEvents.Infra.Data/EntitiesConfiguration/AddressConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using AntonellaEvents.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace AntonellaEvents.Infra.Data.EntitiesConfiguration
+{
+	internal class AddresConfiguration : IEntityTypeConfiguration<Address>
+	{
+		public void Configure(EntityTypeBuilder<Address> builder)
+		{
+			builder.Property(s => s.State)
+				.IsRequired()
+				.HasMaxLength(2);
+
+			builder.Property(z => z.ZipCode)
+				.IsRequired()
+				.HasMaxLength(8);
+
+			builder.Property(s => s.Street)
+				.IsRequired()
+				.HasMaxLength(50);
+
+			builder.Property(n => n.Number)
+				.IsRequired()
+				.HasMaxLength(5);
+
+			builder.Property(c => c.Complement)
+				.IsRequired()
+				.HasMaxLength(100);
+
+			builder.Property(c => c.City)
+				.IsRequired()
+				.HasMaxLength(40);
+
+			builder.Property(s => s.State)
+			.IsRequired()
+			.HasMaxLength(40);
+
+			builder.HasQueryFilter(e => e.Active);
+		}
+	}
+}
---
+++ AntonellaEvents.Infra.Data/EntitiesConfiguration/EventsConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using AntonellaEvents.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AntonellaEvents.Infra.Data.EntitiesConfiguration
+{
+	internal class EventsConfiguration : IEntityTypeConfiguration<Events>
+	{
+		public void Configure(EntityTypeBuilder<Events> builder)
+		{
+			builder.HasKey(e => e.Id);
+	
+			builder.Property(e => e.Name)
+				.IsRequired()
+				.HasMaxLength(100);
+
+			builder.Property(e => e.Description)
+				.IsRequired();
+
+			builder.Property(e => e.StartDate)
+				.IsRequired();
+
+			builder.Property(e => e.EndDate)
+				.IsRequired();
+
+			builder.Property(e => e.Location)
+				.IsRequired()
+				.HasMaxLength(100);
+
+			builder.Property(e => e.IsPublic)
+				.IsRequired();
+
+			builder.HasOne(e => e.Address)
+				.WithOne(a => a.Events) 
+				.HasForeignKey<Events>(e => e.AddresId)
+				.IsRequired();
+
+			builder.HasQueryFilter(e => e.Active);
+		}
+	}
+}
