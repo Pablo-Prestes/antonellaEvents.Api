37274c5833754cc15ef77d8f7769c6af72d7ee24
Pablo Prestes <pablo.henrique.prestes@gmail.com>
2025-03-04 21:19:48 -0300

    Feat(Controller, Domain) - Implementando middleware de erro para tratameto de internos e exceções soltas e ApiReponse

---
+++ AntonellaEvents.Api/Helpers/Middlaware/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Text.Json;
+using ADoJob.Domain.Responses;
+
+namespace Bwise.API.Middleware
+{
+	public class ExceptionMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly ILogger<ExceptionMiddleware> _logger;
+		private readonly IHostEnvironment _env;
+
+		public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+		{
+			_next = next;
+			_logger = logger;
+			_env = env;
+		}
+
+		public async Task InvokeAsync(HttpContext context)
+		{
+			try
+			{
+				await _next(context);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, ex.Message);
+				context.Response.ContentType = "application/json";
+				context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+				ApiResponse<string> response = _env.IsDevelopment() ?
+					new ApiResponse<string>(false, ex.Message, ex.StackTrace, context.Response.StatusCode) :
+					new ApiResponse<string>(false, "Internal server error", "", context.Response.StatusCode);
+
+				var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+				var json = JsonSerializer.Serialize(response, options);
+
+				await context.Response.WriteAsync(json);
+			}
+		}
+	}
+}
---
+++ AntonellaEvents.Domain/Helpers/Reponse/ApiResponse.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ADoJob.Domain.Responses
+{
+	public class ApiResponse<T>
+	{
+		public int StatusCode { get; set; }
+		public bool Success { get; set; }
+		public string Message { get; set; }
+
+		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+		public object? Data { get; set; }
+
+		public ApiResponse(bool success, string message, int statusCode = 200)
+		{
+			Success = success;
+			Message = message;
+			StatusCode = statusCode;
+		}
+
+		public ApiResponse(bool success, string message, T data, int statusCode = 200)
+		{
+			Success = success;
+			Message = message;
+			Data = data;
+			StatusCode = statusCode;
+		}
+
+		public ApiResponse(bool success, string message, List<T> data, int statusCode = 200)
+		{
+			Success = success;
+			Message = message;
+			Data = data;
+			StatusCode = statusCode;
+		}
+	}
+}
